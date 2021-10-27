58d336cf953d29cc00283bec0250e2f8526b0d7d
Yuri M <yurimaia@protonmail.ch>
2021-10-26 23:05:20 -0300

    Update archiveweb.py

--- archiveweb.py
+++ archiveweb.py
@@ -5,15 +5,7 @@ from urllib.parse import urlparse
 from googlesearch import search
 from tqdm import tqdm
 
-banner = ("""
- █████╗ ██████╗  ██████╗██╗  ██╗██╗██╗   ██╗███████╗    ██╗    ██╗███████╗██████╗ 
-██╔══██╗██╔══██╗██╔════╝██║  ██║██║██║   ██║██╔════╝    ██║    ██║██╔════╝██╔══██╗
-███████║██████╔╝██║     ███████║██║██║   ██║█████╗      ██║ █╗ ██║█████╗  ██████╔╝
-██╔══██║██╔══██╗██║     ██╔══██║██║╚██╗ ██╔╝██╔══╝      ██║███╗██║██╔══╝  ██╔══██╗
-██║  ██║██║  ██║╚██████╗██║  ██║██║ ╚████╔╝ ███████╗    ╚███╔███╔╝███████╗██████╔╝
-╚═╝  ╚═╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝  ╚══════╝     ╚══╝╚══╝ ╚══════╝╚═════╝ 
-                                                                            v1.1
-""")
+ver = "1.1"
 
 def parse_args():
     parser = argparse.ArgumentParser(usage='archiveweb.py -t example.com')
@@ -25,41 +17,37 @@ def parse_args():
 def dorks(target):
     print(f'\n\n[+] Target: {target.rstrip()}')
     print('+ Google dorks')
-    data = []
-    
+    data = set()
+
     try:
-        for value in tqdm(search(f'site:{target}', start=0, stop=None, pause=2)):
-            data.append(value)
+        for value in tqdm(search(f'site:{target}', start=0, stop=None, pause=2.0)):
+            data.add(value)
     
     except Exception:
-        if 'HTTP Error 429' in value:
-            print('[!] HTTP Error 429: Too Many Requests')
-            print('[!] try later...')
-        else:
-            print('[!] Error in google search')
+        print('[!] Error in google search')
     
-    return sorted(set(data))
+    return data
 
 def wayback(target):
     print('\n+ Wayback machine')
-    data = []
+    data = set()
     
     try:
         r = get(f'http://web.archive.org/cdx/search/cdx?url={target}/*&output=json&collapse=urlkey')
         for value in tqdm(r.json()):
-            data.append(value[2])
+            data.add(value[2])
     
     except Exception:
         print('[!] Error in wayback machine')
     
-    return sorted(set(data))
+    return data
 
 def only_ext(list, ext):
-    data = []
+    data = set()
     
     for url in list:
         if any(f'.{x}' in url.split('/')[-1] for x in ext):
-            data.append(url.rstrip())
+            data.add(url.rstrip())
     
     if not data:
         print(f'[!] {ext} not found')
@@ -67,21 +55,21 @@ def only_ext(list, ext):
     return data
 
 def remove_ext(list):
-    data = []
+    data = set()
     ext = ('.css', '.png', '.pdf', '.jpg', '.jpeg', '.ico', '.bmp', '.svg', '.gif', '.woff', '.woff2', '.ttf')
     
     for url in list:
         u = urlparse(url)
         if not any(x in u.path.split('/')[-1] for x in ext) and 'http' in url:
-            data.append(url.rstrip())
+            data.add(url.rstrip())
     
     return data
 
 def clear(list):
     print('\n+ Organizing list')
     
-    data = []
-    temp = []
+    data = set()
+    temp = set()
     
     urls = sorted(set(list))
     
@@ -89,14 +77,14 @@ def clear(list):
         u = urlparse(url)
 
         if not u.query:
-            data.append(url.rstrip())
+            data.add(url.rstrip())
         
         if u.query and '&' not in u.query:
             param = u.query.split('=')[0]
             x = f'{u.scheme}{u.netloc}{u.path}{param}'
             if x not in temp:
-                temp.append(x)
-                data.append(url.rstrip())
+                temp.add(x)
+                data.add(url.rstrip())
         
         if '&' in u.query:
             param = u.query.split('&')
@@ -106,8 +94,8 @@ def clear(list):
                 concat += c
             x = f'{u.scheme}{u.netloc}{u.path}{sorted(concat)}'
             if x not in temp:
-                temp.append(x)
-                data.append(url.rstrip())
+                temp.add(x)
+                data.add(url.rstrip())
     
     return data
 
@@ -134,10 +122,10 @@ def main():
                 if args.ext:
                     data = clear([*dorks(target.rstrip()), *wayback(target.rstrip())])
                     data = only_ext(data, args.ext.split(','))
-                    logger(target, data)
+                    logger(target.rstrip(), data)
                 else:
-                    data = clear(remove_ext([*dorks(target), *wayback(target)]))
-                    logger(target, data)
+                    data = clear(remove_ext([*dorks(target.rstrip()), *wayback(target.rstrip())]))
+                    logger(target.rstrip(), data)
 
         else:
             print('[!] No arguments')
@@ -147,5 +135,4 @@ def main():
         print('\n\n[!] Stopping')
 
 if __name__ == '__main__':
-    print(banner)
     main()
