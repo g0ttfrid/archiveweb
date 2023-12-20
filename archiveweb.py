c62763f37271171f00a5a62b28e8da49bec6c40d
g0ttfrid <yurimaia@protonmail.ch>
2023-12-19 22:06:39 -0300

    Update archiveweb.py

--- archiveweb.py
+++ archiveweb.py
@@ -9,13 +9,12 @@ ver = "1.1"
 
 def parse_args():
     parser = argparse.ArgumentParser(usage='archiveweb.py -t example.com')
-    parser.add_argument('-t', '--target', type=str, help='insert domain/subdomain')
-    parser.add_argument('-f', '--file', type=open, help='insert list w/ domains/subdomains')
+    parser.add_argument('-t', '--target', type=str, required=True, help='insert domain/subdomain')
     parser.add_argument('-x', '--ext', type=str, help='get result w/ specific extensions (-x asp,aspx)')
     return parser.parse_args()
 
 def dorks(target):
-    print(f'\n\n[+] Target: {target.rstrip()}')
+    print(f'\n[+] Target: {target.rstrip()}')
     print('+ Google dorks')
     data = set()
 
@@ -23,8 +22,8 @@ def dorks(target):
         for value in tqdm(search(f'site:{target}', start=0, stop=None, pause=3.0)):
             data.add(value)
     
-    except Exception:
-        print('[!] Error in google search')
+    except Exception as err:
+        print(f'[!] Error in google search\n{err}')
     
     return data
 
@@ -37,8 +36,8 @@ def wayback(target):
         for value in tqdm(r.json()):
             data.add(value[2])
     
-    except Exception:
-        print('[!] Error in wayback machine')
+    except Exception as err:
+        print(f'[!] Error in wayback machine\n{err}')
     
     return data
 
@@ -107,29 +106,13 @@ def logger(target, list):
 if __name__ == '__main__':
     try:
         args = parse_args()
-
-        if args.target:
-            if args.ext:
-                data = clear([*dorks(args.target), *wayback(args.target)])
-                data = only_ext(data, args.ext.split(','))
-                logger(args.target, data)
-            else:
-                data = clear(remove_ext([*dorks(args.target), *wayback(args.target)]))
-                logger(args.target, data)
-        
-        elif args.file:
-            for target in args.file:
-                if args.ext:
-                    data = clear([*dorks(target.rstrip()), *wayback(target.rstrip())])
-                    data = only_ext(data, args.ext.split(','))
-                    logger(target.rstrip(), data)
-                else:
-                    data = clear(remove_ext([*dorks(target.rstrip()), *wayback(target.rstrip())]))
-                    logger(target.rstrip(), data)
-
+        if args.ext:
+            data = clear([*dorks(args.target), *wayback(args.target)])
+            data = only_ext(data, args.ext.split(','))
+            logger(args.target, data)
         else:
-            print('[!] No arguments')
-            print('[!] Try archiveweb.py -h')
+            data = clear(remove_ext([*dorks(args.target), *wayback(args.target)]))
+            logger(args.target, data)
     
     except KeyboardInterrupt:
         print('\n\n[!] Stopping')
